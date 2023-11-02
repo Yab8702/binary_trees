3c2c666bee5c9a5e7034b9e119a182bdc14c95a6
= <=>
2023-11-02 08:17:54 +0000

    updated

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,12 +9,10 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node)
+
+	if (!(!node->left && !node->right) || !node)
 		return (0);
 
-	if (!node->left && !node->right)
-		return (1);
-
-	return (0);
+	return (1);
 
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,11 +9,9 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (!node)
+
+	if (!(!node->parent) || !node)
 		return (0);
 
-	if (!node->parent)
-		return (1);
-
-	return (0);
+	return (1);
 }
