0b1a75ae8a52dd7ba582b4e69bd8ebfe9906a34f
= <=>
2023-11-02 08:07:51 +0000

    betty pass updated

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,10 +9,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	
 	if (!node->left && !node->right && node)
 		return (1);
-	
+
 	return (0);
 
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,6 +12,6 @@ int binary_tree_is_root(const binary_tree_t *node)
 
 	if (!node->parent && node)
 		return (1);
-	
+
 	return (0);
 }
